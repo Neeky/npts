109f32eafd3f8d50e72743e1838eefe6f2d2ec0f
蒋乐兴 <1721900707@qq.com>
2023-01-21 13:16:17 +0800

    ✨feature: 实现 cpu 指标的采集

--- src/npts/gathers/cpu.py
+++ src/npts/gathers/cpu.py
@@ -1,18 +1,38 @@
 # -*- coding: utf8 -*-
 
 """实现 cpu 各类指标的采集
+
+作者: 蒋乐兴|neeky
+日期: 2022-01-21
+邮件: 1721900707@qq.com|neeky@live.com
 """
 
 import psutil
-"""
-    def __post_init__(self):
-        self.clt_at = datetime.now()
-        psutil_cpu_times = psutil.cpu_times()
-        if len(psutil_cpu_times) == 4:
-            logging.info("len of psutil.cpu_times() equals 4 mac os .")
-            self.user = psutil_cpu_times.user
-            self.system = psutil_cpu_times.system
-            self.idle = psutil_cpu_times.idle
-        elif len(psutil_cpu_times) == 10:
-            logging.info("len of psutil.cpu_times() equals 10 linux .")
-"""
+from collections import deque
+
+from npts.models.cpu import CpuTimes
+
+
+def current_cpu_times():
+    """返回当前 cpu 的耗时信息
+    """
+    # 核心数量
+    cores = psutil.cpu_count()
+
+    # 频率
+    frequency, *_ = psutil.cpu_freq()
+
+    # 其它监控项
+    current_cpu_time = psutil.cpu_times_percent()
+
+    user = current_cpu_time.user
+    system = current_cpu_time.system
+    idle = current_cpu_time.idle
+    iowait = 0
+    softirq = 0
+    if len(current_cpu_time) == 10:
+        iowait = current_cpu_time.iowait
+        softirq = current_cpu_time.softirq
+    
+    # 创建 CpuTimes 对象并保存到队列
+    return CpuTimes(cores=cores, frequency=frequency, user=user, system=system, idle=idle, iowait=iowait, softirq=softirq)
--- src/npts/models/cpu.py
+++ src/npts/models/cpu.py
@@ -10,14 +10,18 @@ class CpuTimes(object):
     """反应 CPU 的时间消耗分布的指标
     """
     cores: int = 0
+    frequency: float = 0
     user: float = 0
     system: float = 0
     idle: float  = 0
     iowait: float = 0
     softirq: float = 0
-    clt_at: datetime = None
+    gather_at: datetime = None
 
-    def save():
-        logging.debug(f"save CpuTimes to sqlite .")
-
+    def __post_init__(self):
+        """更新对象的采集时间
+        """
+        if self.gather_at is None:
+            self.gather_at = datetime.now()
     
+

7f440f5a4dab937c2f625bf692f8db02519cc5b8
蒋乐兴 <1721900707@qq.com>
2023-01-24 16:15:02 +0800

    🖥️ wip: 调整代码风格

---
+++ src/npts/cores/defaults.py
@@ -0,0 +1,18 @@
+# -*- coding: utf8 -*-
+
+"""
+npts 全局默认值
+
+作者: 蒋乐兴|neeky
+日期: 2022-01-24
+邮件: 1721900707@qq.com|neeky@live.com
+"""
+
+
+PID = "/tmp/npts-daemon.pid"
+MAIN_THREAD_SLEEP_TIME = 11
+
+LOG_FILE_FULL_PATH = "/tmp/npts-daemon.log"
+MSG_FULL_VERSION = "npts-{0} \npowered by neeky \nblogs https://www.sqlpy.com ."
+MSG_LOG_CONFIG_COMPLETED = "logging config completed using log file '{0}' ."
+MSG_DAEMON_IS_RUNNING = "npts-daemon is running version = '{0}' ."
--- src/npts/main.py
+++ src/npts/main.py
@@ -1,5 +1,12 @@
 # -*- coding: utf8 -*-
 
+"""npt 服务入口点
+
+作者: 蒋乐兴|neeky
+日期: 2022-01-24
+邮件: 1721900707@qq.com|neeky@live.com
+"""
+
 import time
 import logging
 import argparse
@@ -7,6 +14,7 @@ from logging.handlers import RotatingFileHandler
 
 from npts.cores.daemon import start_daemon, stop_daemon
 from npts.version import VERSION
+from npts.cores import defaults
 
 
 def parser_args():
@@ -25,28 +33,27 @@ def main():
 
     # 确认运行模式
     if args.action == 'version':
-        print(f"npts-{VERSION} \npowered by neeky \nblogs https://www.sqlpy.com .")
+        print(defaults.MSG_FULL_VERSION.format(VERSION))
         return 0
     elif args.action == 'start':
-        start_daemon("/tmp/npts-daemon.pid")
+        start_daemon(defaults.PID)
     elif args.action == 'stop':
-        stop_daemon('/tmp/npts-daemon.pid')
+        stop_daemon(defaults.PID)
     else:
         return 0
     
     # 配置日志
-    logfile="/tmp/npts-daemon.log"
     levels = {
         'info': logging.INFO,
         'debug': logging.DEBUG,
         'error': logging.ERROR,
         'warn': logging.WARN
         }
-    handler = RotatingFileHandler(filename=logfile, maxBytes=128 * 1024 * 1024, backupCount=8, encoding="utf8")
+    handler = RotatingFileHandler(filename=defaults.LOG_FILE_FULL_PATH, maxBytes=128 * 1024 * 1024, backupCount=8, encoding="utf8")
     logging.basicConfig(handlers=[handler], level=levels[args.log_level], format="[%(asctime)s %(levelname)s] - [%(threadName)s] - [%(pathname)s %(lineno)d line]  ~  %(message)s")
-    logging.info(f"logging config completed using log file '{logfile}' .")
+    logging.info(defaults.MSG_LOG_CONFIG_COMPLETED.format(defaults.LOG_FILE_FULL_PATH))
 
     # 主线程永远在后台运行下去
     while True:
-        logging.info(f"npts-daemon is running .")
-        time.sleep(11)
+        logging.info(defaults.MSG_DAEMON_IS_RUNNING.format(VERSION))
+        time.sleep(defaults.MAIN_THREAD_SLEEP_TIME)
